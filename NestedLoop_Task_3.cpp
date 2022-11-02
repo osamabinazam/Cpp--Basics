c73111d15deab0e988188b35acc737346bf4dfb3
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-02 20:28:02 +0500

    first commit

---
+++ NestedLoop_Task_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Right Triangle Using Numbers
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            /* code */
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
